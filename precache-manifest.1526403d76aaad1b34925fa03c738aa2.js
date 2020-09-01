60f77cd4fa477b1bb2103d48c14453ff498fe657
girijapradhan9925 <46650353+girijapradhan9925@users.noreply.github.com>
2020-09-01 13:11:11 +0530

    Updates

---
+++ precache-manifest.1526403d76aaad1b34925fa03c738aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b46833da3b67d68613fb9abd280a72",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "5ff281a48efda4b35266",
+    "url": "/Todo-React-App/static/css/main.dea0d80f.chunk.css"
+  },
+  {
+    "revision": "d05064560199158a01a2",
+    "url": "/Todo-React-App/static/js/2.6050587a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-React-App/static/js/2.6050587a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff281a48efda4b35266",
+    "url": "/Todo-React-App/static/js/main.14304d96.chunk.js"
+  },
+  {
+    "revision": "3fc85ac0fe65da1636fe",
+    "url": "/Todo-React-App/static/js/runtime-main.3d02d11d.js"
+  }
+]);
